75faec6779269c51dfdcd63b4348fe33096c00b5
ingr.in <aco@ingr.in>
2026-06-18 11:24:12 +0530

    Add initial app.js with nested functions

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+expert default {
+app();
+  function app(){
+    run();
+    function run(){
+      console();
+    }
+    function console(){
+      const msg="Hello <br> This is App Run";
+      console.log(msg);
+    }
+  }
+}
